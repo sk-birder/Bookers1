a571b0157997579e719d1da331541fc3cbefc19f
sk-birder <199427351+sk-birder@users.noreply.github.com>
2025-03-20 03:24:30 +0000

    [Add] 投稿機能を追加 リダイレクト未調整

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,20 @@
 class BooksController < ApplicationController
   def index
+    @booklist = Book.all
+    @booknew = Book.new
   end
 
   def new
   end
 
   def create
+    @booknew = Book.new(book_params)
+    @booknew.save
+    redirect_to book_path(@booknew.id)
   end
 
   def show
+    @book = Book.find(params[:id]) # このparamsがなんなのか理解してない
   end
 
   def edit
@@ -21,6 +27,7 @@ class BooksController < ApplicationController
   end
 
   private
-  def ...
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # root to:  'コントローラ名#アクション名'
   root to: 'homes#top'
   get 'homes/top' # ルートパスでの表示に変更
+  post 'books' => 'books#create'
   resources :books # index, create, new, edit, show, update(PATCH), update(PUT), destroy
 end
